fe7013294741205d5a591063a0d35417b91bac49
johannes <johannesbarja@protonmail.com>
2023-04-14 21:43:02 -0600

    Configure neovim to use 4 spaces for the tab key

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,9 +2,13 @@ require('packer-plugins')
 
 -- Set theme
 vim.opt.termguicolors = true
-vim.o.background = "dark"
+vim.opt.background = "dark"
 vim.cmd.colorscheme('gruvbox')
 
 -- Set more convienent number line
 vim.opt.relativenumber = true
 vim.opt.number = true
+
+-- Use 4 spaces for tab
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
