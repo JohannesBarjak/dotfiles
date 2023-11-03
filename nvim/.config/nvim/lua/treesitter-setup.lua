3df81b18ed726a5f7536cd7f794d381110f5caa1
johannes <johannes@arch.localhost.localdomain>
2023-11-02 22:05:57 -0500

    Add Treesitter grammars

--- nvim/.config/nvim/lua/treesitter-setup.lua
+++ nvim/.config/nvim/lua/treesitter-setup.lua
@@ -1,6 +1,15 @@
 -- Configure treesitter
 require('nvim-treesitter.configs').setup {
-    ensure_installed = { "haskell" },
+    ensure_installed =
+        { "haskell", "agda"
+        , "rust", "c", "nasm"
+        , "python", "lua"
+        , "vim", "vimdoc"
+        , "query", "regex"
+        , "ini", "toml"
+        , "gdscript"
+        },
+
     highlight =
         { enable = true }
 }
