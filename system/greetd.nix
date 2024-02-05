76206baf2221bff4652b318b7ff2e01dc9a16b45
Johannes <johannes.barjak@gmail.com>
2024-02-05 15:43:56 -0600

    Use vt 7 for greetd

--- system/greetd.nix
+++ system/greetd.nix
@@ -1,9 +1,9 @@
 # Run hyprland in a dbus session so that xdg-mime works correctly.
-{...}: let cmd = "dbus-run-session Hyprland &> /dev/null"; {
+{pkgs, ...}: let cmd = "dbus-run-session Hyprland &> /dev/null"; in {
   # Define login manager.
   services.greetd = {
     enable = true;
-    vt = 3;
+    vt = 7;
 
     settings.default_session = {
       command = "${pkgs.greetd.tuigreet}/bin/tuigreet -r -t --asterisks --user-menu --cmd \"${cmd}\"";
