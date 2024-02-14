cad6386ab56da7e50e985bd8fc5c01ec85663d2e
Johannes <johannes.barjak@gmail.com>
2024-02-14 14:04:43 -0600

    Reduce hyprland gap size

--- home/wm/hyprland.nix
+++ home/wm/hyprland.nix
@@ -11,8 +11,8 @@
       "$filemanager" = "nautilus";
 
       general = {
-        gaps_in = 5;
-        gaps_out = 8;
+        gaps_in = 3;
+        gaps_out = 5;
         border_size = 2;
 
         # Set border colors.
