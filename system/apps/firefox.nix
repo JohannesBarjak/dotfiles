c6f4a3202df9ddd9b1b099127a09583f92af839b
Johannes <johannes.barjak@gmail.com>
2024-02-17 10:33:38 -0600

    Add comment explaining extension names in firefox config

--- system/apps/firefox.nix
+++ system/apps/firefox.nix
@@ -7,6 +7,7 @@
       DisableFirefoxStudies = true;
 
       ExtensionSettings = {
+        # Extension names are based on their id which is visible in about:support.
         "uBlock0@raymondhill.net" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
           installation_mode = "force_installed";
