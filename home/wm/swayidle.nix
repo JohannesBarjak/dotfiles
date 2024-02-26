9f80b6665ea8b38c1936bb5e3c94ad10a190a76c
Johannes <johannes.barjak@gmail.com>
2024-02-26 14:29:40 -0600

    Add icon to the suspend warning

--- home/wm/swayidle.nix
+++ home/wm/swayidle.nix
@@ -4,7 +4,7 @@
     enable = true;
 
     timeouts = [
-      { timeout = 300; command = "${pkgs.libnotify}/bin/notify-send 'This computer will suspend soon' -t 15000"; }
+      { timeout = 300; command = "${pkgs.libnotify}/bin/notify-send -i dialog-warning 'This computer will suspend soon' -t 15000"; }
       { timeout = 315; command = "${pkgs.systemd}/bin/systemctl suspend"; }
     ];
 
