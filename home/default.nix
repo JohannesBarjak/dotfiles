143fd642220f08e5dc63269977e37cb5d9194194
Johannes <johannes.barjak@gmail.com>
2024-02-27 23:01:28 -0600

    Add more cli tools and remove config from home/default.nix

--- home/default.nix
+++ home/default.nix
@@ -7,6 +7,7 @@
     ./wm
     ./apps
     ./theming.nix
+    ./terminal.nix
   ];
 
   home.packages = with pkgs; [ numbat ];
@@ -44,65 +45,6 @@
     extraConfig = {
       pull.rebase = true;
     };
-  };
-
-  programs.nushell = {
-    enable = true;
-    package = pkgs.nushellFull;
-    extraConfig = "$env.config = { show_banner: false }";
-  };
-
-    # Enable Nushell autosuggestions using carapace
-  programs.carapace = {
-    enable = true;
-    enableNushellIntegration = true;
-  };
-
-  programs.starship = {
-    enable = true;
-    enableNushellIntegration = true;
-  };
-
-  programs.kitty = {
-    enable = true;
-    theme = "Gruvbox Material Dark Medium";
-
-    settings = {
-      scrollback_lines = 1000;
-      background_opacity = "0.75";
-    };
-
-    font.name = "FiraCode Nerd Font Mono Reg";
-    font.package = ( pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; });
-    font.size = 11;
-  };
-
-  xdg.desktopEntries = {
-    kitty = {
-      name = "kitty";
-      genericName = "Terminal emulator";
-      type = "Application";
-
-      icon = "kitty";
-      exec = "kitty --single-instance";
-      categories = [ "System" "TerminalEmulator" ];
-    };
-  };
-
-  # Use helix as an editor.
-  programs.helix = {
-    enable = true;
-    defaultEditor = true;
-
-    settings = {
-      theme = "gruvbox";
-      editor.line-number = "relative";
-    };
-  };
-
-  programs.btop = {
-    enable = true;
-     settings.color_theme = "gruvbox_material_dark";
   };
 
   home.stateVersion = "23.11";
---
+++ home/terminal.nix
@@ -0,0 +1,61 @@
+{pkgs, ...}: {
+  programs.nushell = {
+    enable = true;
+
+    package = pkgs.nushellFull;
+    extraConfig = "$env.config = { show_banner: false }"; # Removes annoying welcome message.
+  };
+
+  # Carapace completes command arguments.
+  programs.carapace = {
+    enable = true;
+    enableNushellIntegration = true;
+  };
+
+  # Starship provides a pretty prompt for nushell.
+  programs.starship = {
+    enable = true;
+    enableNushellIntegration = true;
+  };
+
+  programs.kitty = {
+    enable = true;
+    theme = "Gruvbox Material Dark Medium";
+
+    settings = {
+      scrollback_lines = 1000;
+      background_opacity = "0.75";
+    };
+
+    font.name = "FiraCode Nerd Font Mono Reg";
+    font.package = ( pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; });
+    font.size = 11;
+  };
+
+  programs.helix = {
+    enable = true;
+    defaultEditor = true;
+
+    settings = {
+      theme = "gruvbox";
+      editor.line-number = "relative";
+    };
+  };
+
+  programs.btop = {
+    enable = true;
+     settings.color_theme = "gruvbox_material_dark";
+  };
+
+  # Tui file manager.
+  programs.yazi = {
+    enable = true;
+    enableNushellIntegration = true;
+  };
+
+  # Zoxide is an autojumping cd.
+  programs.zoxide = {
+    enable = true;
+    enableNushellIntegration = true;
+  };
+}
