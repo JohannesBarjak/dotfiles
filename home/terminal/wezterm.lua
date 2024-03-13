c2714aa160da11a3ea0907c83db73e9cc6b03baf
Johannes <johannes.barjak@gmail.com>
2024-03-13 10:09:12 -0500

    Use subpixel rendering in wezterm

--- home/terminal/wezterm.lua
+++ home/terminal/wezterm.lua
@@ -5,7 +5,8 @@ config.color_scheme = 'Gruvbox Material (Gogh)'
 config.integrated_title_button_style = "Gnome"
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 
-config.font = wezterm.font 'FiraCode'
+config.font = wezterm.font 'FiraCode Nerd Font Mono'
 config.font_size = 11.25
+config.freetype_load_target = "HorizontalLcd"
 
 return config
