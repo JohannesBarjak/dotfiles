877e50e25929953afa1ecb9419ad2601d93d64d1
Johannes <johannes.barjak@gmail.com>
2024-09-30 13:38:30 -0500

    Disable pipewire camera due to battery consuming bug

--- system/sound.nix
+++ system/sound.nix
@@ -6,9 +6,22 @@
 
   services.pipewire = {
     enable = true;
+
     pulse.enable = true;
     jack.enable = true;
+
     alsa.enable = true;
     alsa.support32Bit = true;
+
+    wireplumber = {
+      enable = true;
+      extraConfig = {
+        "10-disable-camera" = {
+          "wireplumber.profiles" = {
+            main."monitor.libcamera" = "disabled";
+          };
+        };
+      };
+    };
   };
 }
