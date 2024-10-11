136b5866313175a117716a4f6c0bcc02849bd470
Johannes <johannes.barjak@gmail.com>
2024-10-11 10:56:58 -0500

    Add emacs and vscode

--- home/terminal/editors.nix
+++ home/terminal/editors.nix
@@ -1,4 +1,22 @@
-{...}: {
+{pkgs, ...}: {
+  programs.emacs = {
+    enable = true;
+    extraPackages = epkgs: [
+      epkgs.meow
+      epkgs.idris2-mode epkgs.haskell-mode
+      epkgs.org
+      epkgs.gruvbox-theme
+      epkgs.lsp-mode epkgs.company
+    ];
+
+    package = pkgs.emacs30-pgtk;
+    extraConfig = builtins.readFile ./init.el;
+  };
+
+  programs.vscode = {
+    enable = true;
+  };
+
   programs.kakoune = {
     enable = true;
   };
