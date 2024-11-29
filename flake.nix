0fcce0e7c3511660512c070f9d6e9fd3f6251c72
Johannes <johannes.barjak@gmail.com>
2024-11-29 16:56:47 -0600

    Switch to the new version of NixOS

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.11";
 
     home-manager = {
       url = "github:nix-community/home-manager";
