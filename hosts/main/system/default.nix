4f123c0e61c36240736a4edb24b9bce792a29511
Johannes <johannes.barjak@gmail.com>
2024-12-06 21:08:09 -0600

    Move more configuration to host agnostic config

--- hosts/main/system/default.nix
+++ hosts/main/system/default.nix
@@ -1,7 +1,6 @@
 {...}: {
   imports = [
     ./sound.nix
-    ./desktop
     ./networking.nix
     ./flatpak-workaround.nix
     ./tlp.nix
--- setups/main/system/default.nix
+++ setups/main/system/default.nix
@@ -1,3 +1,6 @@
 {...}: {
-  imports = [ ./firefox.nix ];
+  imports = [
+    ./desktop
+    ./firefox.nix
+  ];
 }
