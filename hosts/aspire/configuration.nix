675f761ef51bddf7720a9a439f323f99f8a8c4c2
Johannes <johannes.barjak@gmail.com>
2024-12-25 12:45:33 -0600

    Add plymouth to aspire computer

--- hosts/aspire/configuration.nix
+++ hosts/aspire/configuration.nix
@@ -14,6 +14,7 @@
   boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.useOSProber = true;
 
+  boot.plymouth.enable = true;
   boot.supportedFilesystems = [ "ntfs" ];
   fileSystems."/".options = [ "compress-force=lzo" ];
 
