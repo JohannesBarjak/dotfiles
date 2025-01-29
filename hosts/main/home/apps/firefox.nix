b807e204972c706429e33caf850afc13a10675ff
Johannes <johannes.barjak@gmail.com>
2025-01-29 00:34:29 -0600

    Add bookmarks

--- hosts/main/home/apps/firefox.nix
+++ hosts/main/home/apps/firefox.nix
@@ -15,6 +15,20 @@
       settings = {
         "browser.tabs.inTitlebar" = 0;
       };
+
+      bookmarks = [
+        { name = "Syllogimous V4";
+          url  = "https://4skinskywalker.github.io/Syllogimous-v4/Intro";
+        }
+        
+        { name = "Kahne: Multiple Mentality";
+          url  = "http://www.rexresearch.com/kahne/kahne.htm";
+        }
+
+        { name = "CMU Research Internships";
+          url  = "https://www.cmu.edu/scs/s3d/reuse/Research/index.html";
+        }
+      ];
     };
   };
 }
