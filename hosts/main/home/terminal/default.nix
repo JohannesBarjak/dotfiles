885d730f34bef09ba11ca219964aae655a9f6d03
Johannes <johannes.barjak@gmail.com>
2025-02-04 11:27:40 -0600

    Move emacs config

--- hosts/main/home/terminal/default.nix
+++ hosts/main/home/terminal/default.nix
@@ -9,7 +9,22 @@
     gitu
   ];
 
-  # Main text editor.
+  # Main gui text editor.
+  programs.emacs = {
+    enable = true;
+    extraPackages = epkgs: [
+      epkgs.meow
+      epkgs.idris2-mode epkgs.haskell-mode
+      epkgs.org
+      epkgs.gruvbox-theme
+      epkgs.lsp-mode epkgs.company
+    ];
+
+    package = pkgs.emacs30-pgtk;
+    extraConfig = builtins.readFile ./init.el;
+  };
+
+  # Main tui text editor.
   programs.helix = {
     enable = true;
     defaultEditor = true;
--- hosts/main/home/terminal/editors.nix
+++ hosts/main/home/terminal/editors.nix
@@ -1,18 +1,4 @@
 {pkgs, ...}: {
-  programs.emacs = {
-    enable = true;
-    extraPackages = epkgs: [
-      epkgs.meow
-      epkgs.idris2-mode epkgs.haskell-mode
-      epkgs.org
-      epkgs.gruvbox-theme
-      epkgs.lsp-mode epkgs.company
-    ];
-
-    package = pkgs.emacs30-pgtk;
-    extraConfig = builtins.readFile ./init.el;
-  };
-
   programs.vscode = {
     enable = true;
   };
