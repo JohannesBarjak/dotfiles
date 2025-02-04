98a41944811dd5a8af8b9d86ffeef04e8fb2c0aa
Johannes <johannes.barjak@gmail.com>
2025-02-04 14:57:35 -0600

    Add opensnitch rule for timesyncd

--- hosts/main/system/networking.nix
+++ hosts/main/system/networking.nix
@@ -5,7 +5,25 @@
   };
 
   services.mullvad-vpn.enable = true;
-  services.opensnitch.enable = true;
+  services.opensnitch = {
+    enable = true;
+    rules = {
+      systemd-timesyncd = {
+        name = "systemd-timesyncd";
+        enabled = true;
+
+        action = "allow";
+        duration = "always";
+
+        operator = {
+          type = "simple";
+          sensitive = false;
+          operand = "process.path";
+          data = "${pkgs.systemd}/lib/systemd/systemd-timesyncd";
+        };
+      };
+    };
+  };
 
   # Add vpn.
   environment.systemPackages = [ pkgs.openvpn ];
