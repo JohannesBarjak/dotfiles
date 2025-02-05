341d38f1650e3040d7de3889e5e5cd119c05e086
Johannes <johannes.barjak@gmail.com>
2025-02-05 01:49:05 -0600

    Add opensnitch rule

--- hosts/main/system/networking.nix
+++ hosts/main/system/networking.nix
@@ -5,8 +5,11 @@
   };
 
   services.mullvad-vpn.enable = true;
+
   services.opensnitch = {
     enable = true;
+    settings.ProcMonitorMethod = "ebpf";
+
     rules = {
       systemd-timesyncd = {
         name = "systemd-timesyncd";
@@ -17,9 +20,44 @@
 
         operator = {
           type = "simple";
-          sensitive = false;
           operand = "process.path";
+
+          sensitive = false;
           data = "${pkgs.systemd}/lib/systemd/systemd-timesyncd";
+        };
+      };
+
+      nsncd = {
+        name = "nsncd";
+        enabled = true;
+
+        action = "allow";
+        duration = "always";
+        precedence = false;
+
+        operator = {
+          type = "list";
+          operand = "list";
+
+          list = [
+            {
+              type = "simple";
+              operand = "process.path";
+
+              sensitive = false;
+              data = "${pkgs.nsncd}/bin/nsncd";
+              list = null;
+            }
+
+            {
+              type = "simple";
+              operand = "dest.host";
+
+              sensitive = false;
+              data = "ipv4.am.i.mullvad.net";
+              list = null;
+            }
+          ];
         };
       };
     };
