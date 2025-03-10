0b6ce3bfb97ed6f517ee238acb9ad8ebc3a1783c
Johannes <johannes.barjak@gmail.com>
2025-03-10 15:27:56 -0500

    Add avy keybindings to emacs.

--- setups/shared/home/emacs/emacs.d/init.el
+++ setups/shared/home/emacs/emacs.d/init.el
@@ -111,3 +111,24 @@
 
 (load-theme 'gruvbox-dark-medium t)
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
+
+; Add avy keybindings.
+; Got the descriptions from the documentation.
+
+; Input one char, jump to it with a tree.
+(global-set-key (kbd "C-'") 'avy-goto-char-2)
+
+; Input two consecutive chars, jump to the first one with a tree.
+(global-set-key (kbd "C-:") 'avy-goto-char)
+
+; Input an arbitrary amount of consecutive chars, jump to the first one with a tree.
+(global-set-key (kbd "C-;") 'avy-goto-char-timer)
+ 
+; Input zero chars, jump to a line start with a tree.
+(global-set-key (kbd "M-g f") 'avy-goto-line)
+
+; Input zero chars, jump to a word start with a tree.
+(global-set-key (kbd "M-g e") 'avy-goto-word-0)
+
+; Input one char at word start, jump to a word start with a tree.
+(global-set-key (kbd "M-g w") 'avy-goto-word-1)
