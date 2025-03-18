5147eaa71f8ef0e45b7b431e3480cac5f0f97161
Johannes <johannes.barjak@gmail.com>
2025-03-18 17:45:55 -0500

    Remove lsp-mode from emacs packages.

--- setups/shared/home/emacs/default.nix
+++ setups/shared/home/emacs/default.nix
@@ -6,7 +6,7 @@
       epkgs.meow epkgs.avy
       epkgs.idris2-mode epkgs.haskell-mode
       epkgs.nix-mode
-      epkgs.lsp-mode epkgs.company
+      epkgs.company
       epkgs.eat epkgs.vterm
       epkgs.magit
       epkgs.doom-themes
