60da7d2ca514ed670ddc5e6c0d1050b82094b669
Johannes <johannes.barjak@gmail.com>
2025-04-07 22:29:09 -0500

    Use :custom for magit-delta variable setting.

--- setups/shared/home/emacs/emacs.d/init.el
+++ setups/shared/home/emacs/emacs.d/init.el
@@ -126,11 +126,11 @@
 ; Add syntax highlighting to magit diffs.
 (use-package magit-delta
   :after magit
+  :init (magit-delta-mode)
   ; Setting the theme to use for magit diff.
-  :config (setq
-	   magit-delta-default-dark-theme "gruvbox-dark"
-	   magit-delta-default-light-theme "gruvbox")
-  :init (magit-delta-mode))
+  :custom
+  (magit-delta-default-dark-theme "gruvbox-dark")
+  (magit-delta-default-light-theme "gruvbox"))
 
 (use-package haskell-mode
   :init (require 'haskell-mode-autoloads))
