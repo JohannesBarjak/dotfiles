6fa67e7a5fc71d82310bc5444874c51dc226493a
Johannes <johannes.barjak@gmail.com>
2025-04-10 16:14:09 -0500

    Add extra embark-consult package and reorder some packages.

--- setups/shared/home/emacs/default.nix
+++ setups/shared/home/emacs/default.nix
@@ -4,18 +4,19 @@
     enable = true;
     extraPackages = epkgs: [
       epkgs.meow epkgs.avy
+      epkgs.embark
+      epkgs.consult
+      epkgs.embark-consult
+      epkgs.vertico epkgs.orderless
+      epkgs.corfu
+      epkgs.transient
       epkgs.idris2-mode epkgs.haskell-mode
       epkgs.nix-mode
-      epkgs.corfu
       epkgs.eldoc-box epkgs.markdown-mode
       epkgs.eat epkgs.vterm
       epkgs.magit epkgs.magit-delta
       epkgs.doom-themes
-      epkgs.transient
-      epkgs.vertico epkgs.orderless
       epkgs.marginalia
-      epkgs.embark
-      epkgs.consult
       epkgs.try
       epkgs.yasnippet
       epkgs.pandoc-mode
