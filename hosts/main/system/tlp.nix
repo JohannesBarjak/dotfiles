6fe45e92694a1cddc9b1bf05f54f22bd10782031
Johannes <johannes.barjak@gmail.com>
2025-04-26 22:18:17 -0500

    Add more power options to tlp.

--- hosts/main/system/tlp.nix
+++ hosts/main/system/tlp.nix
@@ -5,6 +5,12 @@
     settings = {
       START_CHARGE_THRESH_BAT0 = 80;
       STOP_CHARGE_THRESH_BAT0 = 85;
+
+      AMDGPU_ABM_LEVEL_ON_AC = 0;
+      AMDGPU_ABM_LEVEL_ON_BAT = 3;
+
+      RADEON_DPM_STATE_ON_AC = "performance";
+      RADEON_DPM_STATE_ON_BAT = "battery";
     };
   };
 }
