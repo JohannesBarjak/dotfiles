a06b33ca9c65f00e7bd28a6d739e6431d239f368
Johannes <johannes.barjak@gmail.com>
2025-06-03 12:39:33 -0500

    Format flake.nix.

--- flake.nix
+++ flake.nix
@@ -14,40 +14,41 @@
     nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
   };
 
-  outputs = { nixpkgs, home-manager, nixpkgs-stable, nix-flatpak, ... }: {
-    nixosConfigurations.main = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+  outputs = { nixpkgs, home-manager, nixpkgs-stable, nix-flatpak, ... }:
+    let system = "x86_64-linux"; in {
+          nixosConfigurations.main = nixpkgs.lib.nixosSystem {
+            system = "${system}";
 
-      modules = [
-        ./hosts/main/configuration.nix
+            modules = [
+              ./hosts/main/configuration.nix
 
-        home-manager.nixosModules.home-manager {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.johannes = {
-            imports = [
-              ./hosts/main/home
-              nix-flatpak.homeManagerModules.nix-flatpak
+              home-manager.nixosModules.home-manager {
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+                home-manager.users.johannes = {
+                  imports = [
+                    ./hosts/main/home
+                    nix-flatpak.homeManagerModules.nix-flatpak
+                  ];
+                };
+              }
             ];
           };
-        }
-      ];
-    };
 
-    nixosConfigurations.dell = nixpkgs-stable.lib.nixosSystem {
-      system = "x86_64-linux";
+          nixosConfigurations.dell = nixpkgs-stable.lib.nixosSystem {
+            system = "${system}";
 
-      modules = [
-        ./hosts/dell/configuration.nix
-      ];
-    };
+            modules = [
+              ./hosts/dell/configuration.nix
+            ];
+          };
 
-    nixosConfigurations.aspire = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+          nixosConfigurations.aspire = nixpkgs.lib.nixosSystem {
+            system = "${system}";
 
-      modules = [
-        ./hosts/aspire/configuration.nix
-      ];
-    };
-  };
+            modules = [
+              ./hosts/aspire/configuration.nix
+            ];
+          };
+        };
 }
