dc7de7f773ac6679b187a1bcb5077834d200d1ff
Johannes <johannes.barjak@gmail.com>
2025-06-07 19:37:39 -0500

    Separate consult and avy into another file in my init.el.

--- setups/shared/home/emacs/emacs.d/init.el
+++ setups/shared/home/emacs/emacs.d/init.el
@@ -60,30 +60,8 @@
   (prog-mode-hook . display-line-numbers-mode)
   (prog-mode-hook . electric-pair-mode))
 
-;; Avy allows for fast jumping in buffers.
-(use-package avy
-  :config
-  ;; Define avy embark actions from https://karthinks.com/software/avy-can-do-anything/.
-  (defun avy-action-embark (pt)
-    (unwind-protect
-        (save-excursion
-          (goto-char pt)
-          (embark-act))
-      (select-window (cdr (ring-ref avy-ring 0))))
-    t)
-
-  (setf (alist-get ?. avy-dispatch-alist) 'avy-action-embark)
-
-  :bind
-  ("C-:" . avy-goto-char)
-  ("C-'" . avy-goto-char-2)
-
-  ("M-g e" . avy-goto-word-0)
-  ("M-g w" . avy-goto-word-1)
-  ("M-g l" . avy-goto-line)
-  ("M-j" . avy-goto-char-timer)
-
-  :custom (avy-timeout-seconds 0.3))
+;; Configure packages related to more efficient navigation.
+(load-file (user-file "navigation.el"))
 
 ;; Use eglot as my lsp manager.
 (use-package eglot
@@ -95,45 +73,6 @@
   :custom
   (eglot-autoshutdown t) ; shutdown language server after closing last file.
   (eglot-confirm-server-initiated-edits nil)) ; allow edits without confirmation.
-
-;; Use consult for better searching interfaces.
-(use-package consult
-  :bind
-  ;; Buffer switching using consult.
-  ("C-x b" . consult-buffer)            ; Orig. switch-to-buffer.
-
-  ("M-y" . consult-yank-pop)            ; Orig. yank-pop.
-
-  ("M-g f"   . consult-flymake)
-  ("M-g M-g" . consult-goto-line)       ; Orig. goto-line.
-  ("M-g g"   . consult-goto-line)       ; Orig. goto-line.
-  ("M-g m"   . consult-mark)
-  ("M-g i"   . consult-imenu)
-  ("M-g I"   . consult-imenu-multi)
-
-  ;; Seaching commands.
-  ("M-s d" . consult-fd)
-  ("M-s c" . consult-locate)
-  ("M-s r" . consult-ripgrep)
-  ("M-s l" . consult-line)
-  ("M-s L" . consult-line-multi)
-  ("M-s k" . consult-keep-lines)
-  ("M-s u" . consult-focus-lines)
-
-  ;; Commands for fast register access.
-  ("M-#" . consult-register-load)
-  ("M-'" . consult-register-store)
-  ("C-M-#" . consult-register)
-
-  ("C-c h" . consult-history)
-  ("C-c K" . consult-kmacro)
-  ("C-c i" . consult-info)
-
-  (:map isearch-mode-map
-
-  ;; These keybindings help consult-line detect isearch.
-  ("M-s l" . consult-line)
-  ("M-s L" . consult-line-multi)))
 
 ;; Embark provides actions on buffer targets.
 (use-package embark
---
+++ setups/shared/home/emacs/emacs.d/navigation.el
@@ -0,0 +1,64 @@
+;; Use consult for better searching interfaces.
+(use-package consult
+  :bind
+  ;; Buffer switching using consult.
+  ("C-x b" . consult-buffer)            ; Orig. switch-to-buffer.
+
+  ("M-y" . consult-yank-pop)            ; Orig. yank-pop.
+
+  ("M-g f"   . consult-flymake)
+  ("M-g M-g" . consult-goto-line)       ; Orig. goto-line.
+  ("M-g g"   . consult-goto-line)       ; Orig. goto-line.
+  ("M-g m"   . consult-mark)
+  ("M-g i"   . consult-imenu)
+  ("M-g I"   . consult-imenu-multi)
+
+  ;; Seaching commands.
+  ("M-s d" . consult-fd)
+  ("M-s c" . consult-locate)
+  ("M-s r" . consult-ripgrep)
+  ("M-s l" . consult-line)
+  ("M-s L" . consult-line-multi)
+  ("M-s k" . consult-keep-lines)
+  ("M-s u" . consult-focus-lines)
+
+  ;; Commands for fast register access.
+  ("M-#" . consult-register-load)
+  ("M-'" . consult-register-store)
+  ("C-M-#" . consult-register)
+
+  ("C-c h" . consult-history)
+  ("C-c K" . consult-kmacro)
+  ("C-c i" . consult-info)
+
+  (:map isearch-mode-map
+
+  ;; These keybindings help consult-line detect isearch.
+  ("M-s l" . consult-line)
+  ("M-s L" . consult-line-multi)))
+
+;; Avy allows for fast jumping in buffers.
+(use-package avy
+  :config
+  ;; Define avy embark actions from https://karthinks.com/software/avy-can-do-anything/.
+  (defun avy-action-embark (pt)
+    (unwind-protect
+        (save-excursion
+          (goto-char pt)
+          (embark-act))
+      (select-window (cdr (ring-ref avy-ring 0))))
+    t)
+
+  (setf (alist-get ?. avy-dispatch-alist) 'avy-action-embark)
+
+  :bind
+  ("C-:" . avy-goto-char)
+  ("C-'" . avy-goto-char-2)
+
+  ("M-g e" . avy-goto-word-0)
+  ("M-g w" . avy-goto-word-1)
+  ("M-g l" . avy-goto-line)
+  ("M-j" . avy-goto-char-timer)
+
+  :custom (avy-timeout-seconds 0.3))
+
