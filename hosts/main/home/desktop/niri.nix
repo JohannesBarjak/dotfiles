13dda9f6a85ba9b3b3ccce9753975a96a4bcdf3b
Johannes <johannes.barjak@gmail.com>
2025-06-11 12:35:24 -0500

    Add warpd commands to niri for better mouse control.

--- hosts/main/home/desktop/niri.nix
+++ hosts/main/home/desktop/niri.nix
@@ -21,6 +21,11 @@
         "Mod+E".action = spawn [ "${config.programs.emacs.finalPackage}/bin/emacsclient" "-n" "-c" ];
         "Mod+B".action = spawn [ "${config.programs.librewolf.package}/bin/librewolf" ];
 
+        # Add useful warpd commands to control the mouse using the keyboard.
+        "Mod+G".action = spawn [ "${pkgs.warpd}/bin/warpd" "--grid"   ];
+        "Mod+N".action = spawn [ "${pkgs.warpd}/bin/warpd" "--normal" ];
+        "Mod+M".action = spawn [ "${pkgs.warpd}/bin/warpd" "--hint"   ];
+
         # keybindings to focus on workspaces using numbers.
         "Mod+1".action.focus-workspace = 1;
         "Mod+2".action.focus-workspace = 2;
