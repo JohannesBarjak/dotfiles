3c1746a89210c999a0df63b32d9edd06d137984e
Johannes <johannes.barjak@gmail.com>
2025-07-21 13:36:24 -0500

    Add hook to org mode for visual-line-mode.

--- setups/shared/home/emacs/emacs.d/org.el
+++ setups/shared/home/emacs/emacs.d/org.el
@@ -1,7 +1,8 @@
 ;; Put general org mode tweaks here.
 (use-package org-mode
   :defer t
-  :custom (org-hide-emphasis-markers t))
+  :custom (org-hide-emphasis-markers t)
+  :hook (org-mode-hook . visual-line-mode))
 
 ;; Superstar is a package for pretty org mode bullet points.
 (use-package org-superstar
