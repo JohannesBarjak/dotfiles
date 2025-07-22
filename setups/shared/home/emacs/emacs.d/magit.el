489ca23c9c962c3a02b02af1e755d3ab547bcea6
Johannes <johannes.barjak@gmail.com>
2025-07-22 18:24:27 -0500

    Add flake function to Emacs and fix light theme in delta.

--- setups/shared/home/emacs/emacs.d/magit.el
+++ setups/shared/home/emacs/emacs.d/magit.el
@@ -10,4 +10,13 @@
   ;; Setting the theme to use for magit diff.
   :custom
   (magit-delta-default-dark-theme  "gruvbox-dark")
-  (magit-delta-default-light-theme "gruvbox"))
+  (magit-delta-default-light-theme "gruvbox-light")
+
+  :config
+  (defun my/update-system-flake ()
+    "Automatically update the system flake and commit it to git."
+  (interactive)
+  (shell-command "nix flake update")
+  (magit-call-git "commit" "-m" "Update flake."))
+
+  :bind ("C-c l" . my/update-system-flake))
