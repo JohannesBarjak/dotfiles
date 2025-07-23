4935e5ffa13eebd8a5a335e40701e9a12cd13906
Johannes <johannes.barjak@gmail.com>
2025-07-22 22:10:39 -0500

    Change default wallpaper

--- hosts/main/home/desktop/wallpapers/default.nix
+++ hosts/main/home/desktop/wallpapers/default.nix
@@ -2,7 +2,7 @@
   options.wallpaper = {
     path = lib.mkOption {
       type = lib.types.path;
-      default = ./pixel-castle.png;
+      default = ./skyline.png;
 
       description = "This option stores which wallpaper to use";
     };
