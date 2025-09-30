f4f70a7daba6a53a087152ba058bdbf74a231500
Johannes <johannes.barjak@gmail.com>
2025-09-30 15:39:32 -0500

    Fix error in latex preview configuration.

--- setups/shared/home/emacs/emacs.d/org.el
+++ setups/shared/home/emacs/emacs.d/org.el
@@ -5,13 +5,16 @@
   (org-hide-emphasis-markers t)
 
   ;; Improve the rendering and size of org latex previews.
-  (org-format-latex-options (plist-put org-format-latex-options :scale 0.8))
   (org-preview-latex-default-process 'dvisvgm)
 
   :hook
   (org-mode . visual-line-mode)
   (org-mode . org-indent-mode)
-  (org-mode . org-fragtog-mode))
+  (org-mode . org-fragtog-mode)
+
+  :config
+  (setq org-format-latex-options
+        (plist-put org-format-latex-options :scale 0.8)))
 
 ;; Superstar is a package for pretty org mode bullet points.
 (use-package org-superstar
