a81a489b08484075da4628f7de042bb49798ffb0
Johannes <johannes.barjak@gmail.com>
2025-09-30 15:34:57 -0500

    Improve latex experience in Emacs.

--- setups/shared/home/emacs/emacs.d/org.el
+++ setups/shared/home/emacs/emacs.d/org.el
@@ -1,7 +1,13 @@
 ;; Put general org mode tweaks here.
 (use-package org-mode
   :defer t
-  :custom (org-hide-emphasis-markers t)
+  :custom
+  (org-hide-emphasis-markers t)
+
+  ;; Improve the rendering and size of org latex previews.
+  (org-format-latex-options (plist-put org-format-latex-options :scale 0.8))
+  (org-preview-latex-default-process 'dvisvgm)
+
   :hook
   (org-mode . visual-line-mode)
   (org-mode . org-indent-mode)
