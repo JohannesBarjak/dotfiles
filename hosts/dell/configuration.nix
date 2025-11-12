f0580be349b9e41c68910f83331451715f344760
Johannes <johannes.barjak@gmail.com>
2025-11-11 21:22:33 -0600

    Add network manager applications to dell computer.

--- hosts/dell/configuration.nix
+++ hosts/dell/configuration.nix
@@ -113,7 +113,9 @@
     description = "Computer";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
-    #  thunderbird
+      #  thunderbird
+      networkmanagerapplet
+      networkmanager
     ];
   };
 
