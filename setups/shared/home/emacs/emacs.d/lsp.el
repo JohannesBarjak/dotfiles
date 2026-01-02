1501eaf453cc32a0736d6d827cab92db718509df
Johannes <johannes.barjak@gmail.com>
2026-01-02 01:15:18 -0600

    Add hook to disable ligatures when in terminal in Emacs.

--- setups/shared/home/emacs/emacs.d/lsp.el
+++ setups/shared/home/emacs/emacs.d/lsp.el
@@ -37,7 +37,13 @@
 ;; Fira Code font for Emacs.
 ;; Remember to run fira-code-mode-install-fonts so that ligatures are rendered.
 (use-package fira-code-mode
-  :hook prog-mode
+  :hook
+  (after-make-frame-functions . (lambda (frame)
+                                  (with-selected-frame frame
+                                    (if (display-graphic-p)
+                                        (global-fira-code-mode 1)
+                                      (global-fira-code-mode -1)))))
+
   :custom (fira-code-mode-disabled-ligatures '("x")))
 
 ;; Idris2 mode.
