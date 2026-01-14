e6eea47248d992a280105c880162d13d21af8380
Johannes <johannes.barjak@gmail.com>
2026-01-13 21:41:09 -0600

    Get J to compile again.

--- setups/shared/home/default.nix
+++ setups/shared/home/default.nix
@@ -7,7 +7,10 @@
     pkgs.lyx
     pkgs.idris2
     pkgs.idris2Packages.idris2Lsp pkgs.idris2Packages.pack
-    pkgs.j
+    (pkgs.j.overrideAttrs (oldAttrs: {
+      NIX_CFLAGS_COMPILE = " -std=gnu17 -Wno-error";
+      NIX_CPPFLAGS_COMPILE = " -include stdint.h";
+    }))
     pkgs.uiua
     pkgs.cbqn-replxx pkgs.bqn386
   ];
