38b0bc0e4b07510a118b3aa543a1867562cb6689
Johannes <johannes.barjak@gmail.com>
2026-02-01 22:22:00 -0600

    Add code for more complex opensnitch rules.

--- setups/main/system/opensnitchd.nix
+++ setups/main/system/opensnitchd.nix
@@ -7,6 +7,45 @@ let simple_rules = [
       { name = "mullvad-daemon";
         data = "${pkgs.mullvad}/bin/.mullvad-daemon-wrapped";
       }
+    ];
+    multi_rules = [
+      { name = "systemd-timesyncd";
+        list = [
+          {
+            type = "regexp";
+            operand = "dest.host";
+            data = "\\d\\.nixos\\.pool\\.ntp\\.org";
+          }
+
+          {
+            type = "simple";
+            operand = "process.path";
+            data = "${pkgs.systemd}/lib/systemd/systemd-timesyncd";
+          }
+        ];
+      }
+
+      { name = "kworker-wg";
+        list = [
+          {
+            type = "simple";
+            operand = "dest.port";
+            data = "51820";
+          }
+
+          {
+            type = "simple";
+            operand = "user.id";
+            data = "0";
+          }
+
+          {
+            type = "simple";
+            operand = "process.path";
+            data = "Kernel connection";
+          }
+        ];
+      }
     ]; in
 {
   services.opensnitch = {
@@ -14,91 +53,38 @@ let simple_rules = [
     settings.ProcMonitorMethod = "ebpf";
 
     rules = lib.mkMerge [
-      builtins.listToAttrs (map (rule:
-        lib.nameValuePair rule.name {
-          name = rule.name;
-          enabled = true;
+      # Simple rules that match on the path of the executable.
+      (lib.listToAttrs (map (rule: lib.nameValuePair rule.name {
+       name = rule.name;
+       enabled = true;
 
-          action = "allow";
-          duration = "always";
+       action = "allow";
+       duration = "always";
 
-          operator = {
-            type = "simple";
-            operand = "process.path";
+       operator = {
+         type = "simple";
+         operand = "process.path";
 
-            sensitive = true;
-            data = rule.data;
-          };
-        }) simple_rules)
-      {
+         sensitive = true;
+         data = rule.data;
+       };
+     }) simple_rules))
 
-        # Allow timesyncd to get time information from the Nixos ntp pool.
-        systemd-timesyncd = {
-          name = "systemd-timesyncd";
-          enabled = true;
+      # Case-sensitive rules that match more complex conditions.
+      (lib.listToAttrs (map (rule: lib.nameValuePair rule.name {
+       name = rule.name;
+       enabled = true;
 
-          action = "allow";
-          duration = "always";
+       action = "allow";
+       duration = "always";
 
-          operator = {
-            type = "list";
-            operand = "list";
+       operator = {
+         type = "list";
+         operand = "list";
 
-            list = [
-              {
-                type = "regexp";
-                operand = "dest.host";
-
-                sensitive = true;
-                data = "\\d\\.nixos\\.pool\\.ntp\\.org";
-              }
-
-              {
-                type = "simple";
-                operand = "process.path";
-
-                sensitive = true;
-                data = "${pkgs.systemd}/lib/systemd/systemd-timesyncd";
-              }
-            ];
-          };
-        };
-
-        # Add rule to allow wireguard kernel connections.
-        kworker-wg = {
-          name = "kworker-wg";
-          enabled = true;
-
-          action = "allow";
-          duration = "always";
-
-          operator = {
-            type = "list";
-            operand = "list";
-            sensitive = true;
-
-            list = [
-              {
-                type = "simple";
-                operand = "dest.port";
-                data = "51820";
-              }
-
-              {
-                type = "simple";
-                operand = "user.id";
-                data = "0";
-              }
-
-              {
-                type = "simple";
-                operand = "process.path";
-                data = "Kernel connection";
-              }
-            ];
-          };
-        };
-      }
+         list = map (lib.mergeAttrs { sensitive = true; }) rule.list;
+       };
+     }) multi_rules))
     ];
   };
 }
