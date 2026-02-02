51cd5a805041304208927f67965c1ec7982b2b84
Johannes <johannes.barjak@gmail.com>
2026-02-01 20:25:34 -0600

    Add code to reduce attribute declarations in opensnitchd.nix.

--- setups/main/system/opensnitchd.nix
+++ setups/main/system/opensnitchd.nix
@@ -1,128 +1,104 @@
-{pkgs, ...}: {
+{lib, pkgs, ...}:
+# These are simple allow rules that match to an exact binary.
+let simple_rules = [
+      { name = "dhcpcd"; data = "${pkgs.dhcpcd}/bin/dhcpcd"; }
+      { name = "nsncd"; data = "${pkgs.nsncd}/bin/nsncd"; }
+
+      { name = "mullvad-daemon";
+        data = "${pkgs.mullvad}/bin/.mullvad-daemon-wrapped";
+      }
+    ]; in
+{
   services.opensnitch = {
     enable = true;
     settings.ProcMonitorMethod = "ebpf";
 
-    rules = {
+    rules = lib.mkMerge [
+      builtins.listToAttrs (map (rule:
+        lib.nameValuePair rule.name {
+          name = rule.name;
+          enabled = true;
 
-      # Allow timesyncd to get time information from the Nixos ntp pool.
-      systemd-timesyncd = {
-        name = "systemd-timesyncd";
-        enabled = true;
+          action = "allow";
+          duration = "always";
 
-        action = "allow";
-        duration = "always";
+          operator = {
+            type = "simple";
+            operand = "process.path";
 
-        operator = {
-          type = "list";
-          operand = "list";
+            sensitive = true;
+            data = rule.data;
+          };
+        }) simple_rules)
+      {
 
-          list = [
-            {
-              type = "regexp";
-              operand = "dest.host";
+        # Allow timesyncd to get time information from the Nixos ntp pool.
+        systemd-timesyncd = {
+          name = "systemd-timesyncd";
+          enabled = true;
 
-              sensitive = true;
-              data = "\\d\\.nixos\\.pool\\.ntp\\.org";
-            }
+          action = "allow";
+          duration = "always";
 
-            {
-              type = "simple";
-              operand = "process.path";
+          operator = {
+            type = "list";
+            operand = "list";
 
-              sensitive = true;
-              data = "${pkgs.systemd}/lib/systemd/systemd-timesyncd";
-            }
-          ];
+            list = [
+              {
+                type = "regexp";
+                operand = "dest.host";
+
+                sensitive = true;
+                data = "\\d\\.nixos\\.pool\\.ntp\\.org";
+              }
+
+              {
+                type = "simple";
+                operand = "process.path";
+
+                sensitive = true;
+                data = "${pkgs.systemd}/lib/systemd/systemd-timesyncd";
+              }
+            ];
+          };
         };
-      };
 
-      # Nsncd rules.
-      nsncd = {
-        name = "nsncd";
-        enabled = true;
+        # Add rule to allow wireguard kernel connections.
+        kworker-wg = {
+          name = "kworker-wg";
+          enabled = true;
 
-        action = "allow";
-        duration = "always";
-        precedence = false;
+          action = "allow";
+          duration = "always";
 
-        operator = {
-          type = "simple";
-          operand = "process.path";
+          operator = {
+            type = "list";
+            operand = "list";
+            sensitive = true;
 
-          sensitive = true;
-          data = "${pkgs.nsncd}/bin/nsncd";
+            list = [
+              {
+                type = "simple";
+                operand = "dest.port";
+                data = "51820";
+              }
+
+              {
+                type = "simple";
+                operand = "user.id";
+                data = "0";
+              }
+
+              {
+                type = "simple";
+                operand = "process.path";
+                data = "Kernel connection";
+              }
+            ];
+          };
         };
-      };
-
-      # Allow mullvad daemon to establish connections.
-      mullvad-daemon = {
-        name = "mullvad-daemon";
-        enabled = true;
-
-        action = "allow";
-        duration = "always";
-
-        operator = {
-          type = "simple";
-          operand = "process.path";
-
-          sensitive = true;
-          data = "${pkgs.mullvad}/bin/.mullvad-daemon-wrapped";
-        };
-      };
-
-      # Add rule to allow wireguard kernel connections.
-      kworker-wg = {
-        name = "kworker-wg";
-        enabled = true;
-
-        action = "allow";
-        duration = "always";
-
-        operator = {
-          type = "list";
-          operand = "list";
-          sensitive = true;
-
-          list = [
-            {
-              type = "simple";
-              operand = "dest.port";
-              data = "51820";
-            }
-
-            {
-              type = "simple";
-              operand = "user.id";
-              data = "0";
-            }
-
-            {
-              type = "simple";
-              operand = "process.path";
-              data = "Kernel connection";
-            }
-          ];
-        };
-      };
-
-      # Enable dhcpcd.
-      dhcpcd = {
-        name = "dhcpcd";
-        enabled = true;
-
-        action = "allow";
-        duration = "always";
-
-        operator = {
-          type = "simple";
-          operand = "process.path";
-
-          sensitive = true;
-          data = "${pkgs.dhcpcd}/bin/dhcpcd";
-        };
-      };
-    };
+      }
+    ];
   };
 }
