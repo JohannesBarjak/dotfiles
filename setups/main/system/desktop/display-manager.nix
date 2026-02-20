c8c9783c026156fec500820a2eda9ae1a9b60288
Johannes <johannes.barjak@gmail.com>
2026-02-19 19:01:10 -0600

    No longer use a display manager.

--- setups/main/system/desktop/display-manager.nix
+++ setups/main/system/desktop/display-manager.nix
@@ -1,42 +1,19 @@
-{config, pkgs, ...}: with config; let
-  cmd = "${programs.uwsm.package}/bin/uwsm start ${sessionData}/mango-uwsm.desktop";
-  sessionData = "${services.displayManager.sessionData.desktops}/share/wayland-sessions"; in {
-    services.greetd = {
-      enable = true;
+{config, pkgs, ...}: {
+  # Autologin and window manager startup.
+  services.getty.autologinUser = "johannes";
 
-      settings = {
-        # Autologin into niri.
-        initial_session = {
-          user = "johannes";
-          command = cmd;
+  environment.loginShellInit =  with config.programs; ''
+    if [ "$(tty)" = "/dev/tty1" ]; then
+      ${uwsm.package}/bin/uwsm start mango-uwsm.desktop
+    fi
+  '';
 
-        };
-
-        default_session.command =
-          "${pkgs.tuigreet}/bin/tuigreet -r -t --asterisks --user-menu --sessions ${sessionData} --cmd '${cmd}'";
-      };
+  programs.uwsm = {
+    enable = true;
+    waylandCompositors.mango = {
+      binPath = "${pkgs.mangowc}/bin/mango";
+      prettyName = "MangoWC";
+      comment = "A feature rich Wayland compositor.";
     };
-
-    programs.uwsm = {
-      enable = true;
-      waylandCompositors.mango = {
-        binPath = "${pkgs.mangowc}/bin/mango";
-        prettyName = "MangoWC";
-        comment = "A feature rich Wayland compositor.";
-      };
-    };
-
-    # Got this from 'https://github.com/apognu/tuigreet/issues/68'
-    # Prevent tty text from appearing on greetd
-    systemd.services.greetd.serviceConfig = {
-      Type = "idle";
-      StandardInput = "tty";
-      StandardOutput = "tty";
-      StandardError = "journal";
-      TTYReset = true;
-      TTYVHangup = true;
-      TTYVTDisallocate = true;
-    };
-
-    systemd.services.greetd.environment.RUST_BACKTRACE = "full";
-  }
+  };
+}
