9709949f2b75ba2b54d6df34ee937eca4ffe7515
Johannes <johannes.barjak@gmail.com>
2026-04-14 22:14:43 -0500

    Fix j package again.

--- home/default.nix
+++ home/default.nix
@@ -15,6 +15,9 @@
     (pkgs.j.overrideAttrs (oldAttrs: {
       NIX_CFLAGS_COMPILE = " -std=gnu17 -Wno-error";
       NIX_CPPFLAGS_COMPILE = " -include stdint.h";
+
+      nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [ pkgs.prelink ];
+      postFixup = (oldAttrs.postFixup or "") + "execstack -c $out/bin/libj.so";
     }))
     pkgs.uiua
     pkgs.cbqn-replxx pkgs.bqn386
